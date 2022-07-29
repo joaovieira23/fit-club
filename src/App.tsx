0d01a15d74789965f6bf2a7295eeb7cebcb961a4
Andrade <joaovictorvieira.23@hotmail.com>
2022-07-29 13:48:00 -0300

    chore: configure project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span style={{ color: 'white' }}>João Andrade</span>
+    </div>
+  );
+}
+
+export default App;
