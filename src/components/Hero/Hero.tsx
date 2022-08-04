52e203d80b96c2c8ffa7457559d6e639dfc7f249
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-04 20:15:15 -0300

    style: style buttons

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -42,6 +42,12 @@ const Hero = () => {
                         <span>fitness programs</span>
                     </div>
                 </div>
+
+                {/* Hero buttons */}
+                <div className="hero-buttons">
+                    <button className="btn">Get Started</button>
+                    <button className="btn">Learn More</button>
+                </div>
             </div>
             <div className="right-h">
                 right-side
